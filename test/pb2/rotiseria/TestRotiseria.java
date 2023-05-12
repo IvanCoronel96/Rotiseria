333e7af2f5d70edf7e1e4179ebcc49ecef8c5b39
Ivan Coronel <imc8046@gmail.com>
2023-05-12 12:08:20 -0300

    Base del proyecto

---
+++ test/pb2/rotiseria/TestRotiseria.java
@@ -0,0 +1,15 @@
+package pb2.rotiseria;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRotiseria {
+
+	@Test
+	public void test() {
+		//Primer commit
+		fail("Not yet implemented");
+	}
+
+}
